b707e2a19f727db3f3c93d53511e2646f2c8d9f0
Veronicaslater86 <karima.krachai@gmail.com>
2021-03-10 15:57:39 +0100

    mise à jour adresse api et ajout fichier readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Widget API LOTR
+
+Widget utilisant l'API LOTR permettant d'afficher et de filtrer la liste des personnages.
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 var search = "character";
 const list = document.getElementById("ring")
-const URLy = `https://the-one-api.herokuapp.com/v1/${search}`;
-const URLquote = `https://the-one-api.herokuapp.com/v1/${search}/${idName}/quote`
+const URLy = `https://the-one-api.dev/v2/${search}`;
+const URLquote = `https://the-one-api.dev/v2/${search}/${idName}/quote`
 const quote = document.getElementById("quote");
 const div = document.getElementById("lord");
 var nameOfCharacters = null;
@@ -9,13 +9,19 @@ var idName = null;
 const token = "u3koBWOmNFkm7IIb5QVw";
 var recherche = null;
 var tabIdFilms = [];
-const URLmovies = `https://the-one-api.herokuapp.com/v1/movie`;
+const URLmovies = `https://the-one-api.dev/v2/movie`;
 const TheReturnoftheKing = "5cd95395de30eff6ebccde5d";
 const TheFellowshipoftheRing = "5cd95395de30eff6ebccde5c";
 const TheTwoTowers = "5cd95395de30eff6ebccde5b";
 const lord = document.getElementById("lord")
-
-
+const btnRappel = document.getElementById("btnRappel")
+const inputRace = document.getElementsByName("race");
+const irace = document.querySelector('input[name="race"]').value
+const btn = document.getElementById("btn")
+var valeur;
+console.log(irace)
+console.log(btn)
+console.log(inputRace)
 
 // const inputRace = document.querySelector();
 const all = document.getElementById("all")
@@ -37,35 +43,85 @@ function start() {
 
 function display(response) {
 	// list.innerHTML = "";
+	allCharacters(response)
+	btn.onclick = ()=> {
+		for(let i = 0; i < inputRace.length; i++){
+			if(inputRace[i].checked){
+			valeur = inputRace[i].value;
+			console.log(valeur)
+			} 
+		   }
+		  allCharacters(funcFiltreRace(response,valeur));
+		}
+function allCharacters (response){
 	all.innerHTML = "";
-
+	var i = 0
 	response.forEach(element => {
+		var div = document.createElement('div');
+		all.appendChild(div);
 		var name = element.name
 		var birth = element.birth;
 		var death = element.death;
 		var race = element.race;
-		all.innerHTML +=
-		 ` <div><h3>${name}</h3>
+		
+		div.innerHTML +=
+			` <h3>${name}</h3>
 			<li>Race : ${race}</li>
 			<li>Naissance : ${birth}</li>
 			<li>death ${death}</li>
-			<a href="${element.wikiUrl}" ><input class="button" type="button" value="Page wiki"></a></div>`
+			<a href="${element.wikiUrl}" ><input class="button" type="button" value="Page wiki"></a>
+			<div id="overlay" class="overlay">
+			<div id="popup" class="popup">
+			<h2>
+			Liste de quotes de ${name}
+			</h2>
+			<span id="buttonClose" class="btnClose">Close</span>
+			<div id="citations">
+			</div>
+			</div>
+			</div>
+			`
+
+		var buttonQuote = document.createElement("button")
+		buttonQuote.textContent = "Quote"
+		buttonQuote.setAttribute('class', 'btnPopup')
+		buttonQuote.setAttribute('id', 'btnPopup')
+		div.appendChild(buttonQuote);
+
+		const btnClose  = document.querySelector(".btnClose")
+		// var buttonClose = document.createElement("span")
+		// buttonClose.textContent = "Close";
+		// buttonClose.setAttribute('id', 'btnClose');
+		// buttonClose.setAttribute('class', 'btnClose')
+		var overlay = document.getElementById('overlay');
+		var popUp = document.getElementById('popup')
+		// popUp.appendChild(buttonClose)
+		buttonQuote.onclick = (evt) => {
+			console.log(evt.target, name)
+			openMoadl();
+		}
+
+
+		function openMoadl() {
+			overlay.style.display = 'block';
+		}
+
+		btnClose.addEventListener('click', closeModal);
+		function closeModal() {
+			overlay.style.display = 'none';
+		}
+		})
 		
-	})
+}
 
-
-
+function search(response){
 	document.getElementById("personnage").oninput = evt => {
 		list.innerHTML = ""
-		all.innerHTML=""
+		all.innerHTML = ""
 		lord.classList.toggle("active")
-		// all.classList.toggle("no_active")
-		const filtrePersonnage = (element) => element.filter(element => element.name.toLowerCase().match(evt.target.value.toLowerCase()));
-
-
-		filtrePersonnage(response).forEach(element => {
-
-
+		
+		console.log("all >>", all)
+		filtrePersonnage(response,evt.target.value).forEach(element => {
 			console.log(element._id, element);
 			idName = element._id;
 			let name = element.name
@@ -76,29 +132,29 @@ function display(response) {
 			var wikiUrl = element.wikiUrl
 
 			all.innerHTML += ` 
-			<div><h3>${name}</h3>
+			<div>
+			<h3>${name}</h3>
 			<li>Race : ${race}</li>
 			<li>Naissance : ${naissance}</li>
 			<li>death ${mort}</li>
 			<li>Concubin : ${concubin}</li>
 			<a href="${wikiUrl}" ><input class="button" type="button" value="Page wiki"></a></div>`
-			
-			
-			// var newLI = document.createElement("li");
-			// var newContent = document.createTextNode(`Nom : ${rechercheName}, Naissance : ${naissance}, Mort : ${mort}, Race : ${race}, Concubin : ${concubin}, Wiki : ${wikiUrl}`);
-			// list.appendChild(newLI);
-			// newLI.appendChild(newContent);
 			displayQuote()
-
+			if (evt.target.value == ""){
+				all.innerHTML = ""
+			}
 
 		});
 		console.log(filtrePersonnage)
 	};
+}
+	search(response);
+
 	console.log(`id >>>> ${idName}`)
 }
 
 function displayQuote() {
-	const URLquote = `https://the-one-api.herokuapp.com/v1/${search}/${idName}/quote`
+	const URLquote = `https://the-one-api.dev/v2/${search}/${idName}/quote`
 	console.log(URLquote)
 	axios
 		.get(URLquote, {
@@ -137,22 +193,22 @@ function funcQuote(obj) {
 start();
 
 
-function movies() {
-	console.log("https://the-one-api.herokuapp.com/v1/movie")
+// function movies() {
+// 	console.log("https://the-one-api.dev/v2/movie")
 
-	axios
-		.get(URLmovies, {
-			headers: {
-				'Authorization': `Bearer ${token}`
-			}
-		})
-		.then((resp) => {
-			console.log(resp.data.docs);
-			iDFilm(resp.data.docs);
-		})
-		.catch((err) => console.error(err));
-};
-movies();
+// 	axios
+// 		.get(URLmovies, {
+// 			headers: {
+// 				'Authorization': `Bearer ${token}`
+// 			}
+// 		})
+// 		.then((resp) => {
+// 			console.log(resp.data.docs);
+// 			iDFilm(resp.data.docs);
+// 		})
+// 		.catch((err) => console.error(err));
+// };
+// movies();
 
 function iDFilm(element) {
 	element.forEach(el => tabIdFilms.push(el.name));
@@ -167,10 +223,19 @@ function filtreRace(evt) {
 
 }
 
+const filtrePersonnage = (element,search) => element.filter(element => element.name.toLowerCase().match(search.toLowerCase()));
+const funcFiltreRace = (element,search) => {
+	var tab = []
+	element.forEach(el => {
+		if(el.race == search){
+			tab.push(el)
+			}
+	})
+	console.log('>>>>>>>', tab)
+		return tab
 
-
-
-
+	}
+	
 
 // $.ajax({
 // 	url: 'http://localhost:8080/resourceserver/protected-no-scope',
